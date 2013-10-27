eecf4456f880f802eff05895347cb51e3cfd832d
Mateusz Szczap <mszczap@team.mobile.de>
2013-10-27 22:10:48 +0100

    upgraded example to 1.14.0 release
    Change-Id: I916a3e59e8c021913bf581a1cfe7b469b7ca42ea

---
+++ src/main/webapp/js/page-behaviour.js
@@ -0,0 +1,29 @@
+var APP = {}
+
+APP.words = []
+
+APP.pushNewWord = function() {
+    var newWord = $('#newWordTextField').val();
+    APP.words.push(newWord);
+    $('#clientWords').html(soy.example.clientWords({ words: APP.words }));
+}
+
+$(document).ready(function() {
+    $('#getServerTimeLink').click(function() {
+        $.ajax({
+            url: "/spring-soy-view-example/app/server-time",
+            context: document.body,
+            success: function(data) {
+                $('#serverTime').html(data);
+            }
+        });
+    });
+
+    $('#submitButton').click(APP.pushNewWord);
+
+    $('#wordsForm').submit(function() {
+        APP.pushNewWord();
+        return false;
+    });
+
+});
