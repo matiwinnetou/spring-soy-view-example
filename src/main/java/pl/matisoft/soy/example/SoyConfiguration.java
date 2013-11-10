30c079e528af400b008c9184d28bb4957b53fdc3
Mateusz Szczap <mszczap@team.mobile.de>
2013-11-10 17:10:02 +0100

    updated to reflect latest changes in master
    Change-Id: Id419b1ea6c2876a243b3f5c45e3df76272e3b590

--- src/main/java/pl/matisoft/soy/example/SoyConfiguration.java
+++ src/main/java/pl/matisoft/soy/example/SoyConfiguration.java
@@ -1,43 +1,20 @@
 package pl.matisoft.soy.example;
 
+import javax.annotation.Resource;
+
 import com.google.common.collect.Lists;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.json.MappingJacksonJsonView;
-import pl.matisoft.soy.SoyTemplateViewResolver;
-import pl.matisoft.soy.ajax.SoyAjaxController;
 import pl.matisoft.soy.ajax.auth.AuthManager;
 import pl.matisoft.soy.ajax.auth.ConfigurableAuthManager;
-import pl.matisoft.soy.ajax.hash.HashFileGenerator;
-import pl.matisoft.soy.ajax.hash.MD5HashFileGenerator;
-import pl.matisoft.soy.ajax.process.OutputProcessor;
-import pl.matisoft.soy.ajax.process.google.GoogleClosureOutputProcessor;
-import pl.matisoft.soy.ajax.url.DefaultTemplateUrlComposer;
-import pl.matisoft.soy.ajax.url.TemplateUrlComposer;
-import pl.matisoft.soy.bundle.EmptySoyMsgBundleResolver;
-import pl.matisoft.soy.holder.CompiledTemplatesHolder;
-import pl.matisoft.soy.holder.DefaultCompiledTemplatesHolder;
-import pl.matisoft.soy.compile.DefaultTofuCompiler;
-import pl.matisoft.soy.compile.TofuCompiler;
-import pl.matisoft.soy.data.DefaultToSoyDataConverter;
-import pl.matisoft.soy.data.ToSoyDataConverter;
-import pl.matisoft.soy.global.compile.CompileTimeGlobalModelResolver;
-import pl.matisoft.soy.global.compile.EmptyCompileTimeGlobalModelResolver;
-import pl.matisoft.soy.global.runtime.DefaultGlobalModelResolver;
-import pl.matisoft.soy.global.runtime.GlobalModelResolver;
-import pl.matisoft.soy.global.runtime.resolvers.*;
-import pl.matisoft.soy.locale.LocaleProvider;
-import pl.matisoft.soy.locale.SpringLocaleProvider;
-import pl.matisoft.soy.render.DefaultTemplateRenderer;
-import pl.matisoft.soy.render.TemplateRenderer;
-import pl.matisoft.soy.template.DefaultTemplateFilesResolver;
+import pl.matisoft.soy.data.adjust.EmptyModelAdjuster;
+import pl.matisoft.soy.data.adjust.ModelAdjuster;
 
 /**
  * Created with IntelliJ IDEA.
@@ -46,167 +23,43 @@ import pl.matisoft.soy.template.DefaultTemplateFilesResolver;
  * Time: 23:02
  */
 @Configuration
+@ImportResource({"classpath:spring-soy-view-ajax-config.xml"})
+@PropertySource("classpath:/spring-soy-view-example.properties")
 @EnableWebMvc
 @ComponentScan(basePackages = {"pl.matisoft.soy.example"})
 public class SoyConfiguration extends WebMvcConfigurerAdapter {
 
-    @Autowired
-    private javax.servlet.ServletContext context;
-
-    private boolean debugOn = false;
+    @Resource(name = "soyViewResolver")
+    private ViewResolver viewResolver;
 
     @Bean
-    public DefaultTemplateFilesResolver templateFilesResolver() throws Exception {
-        final DefaultTemplateFilesResolver defaultTemplateFilesResolver = new DefaultTemplateFilesResolver();
-        defaultTemplateFilesResolver.setDebugOn(debugOn);
-        defaultTemplateFilesResolver.setRecursive(true);
-        defaultTemplateFilesResolver.setServletContext(context);
-        defaultTemplateFilesResolver.afterPropertiesSet();
-
-        return defaultTemplateFilesResolver;
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
     }
 
     @Bean
-    public CompileTimeGlobalModelResolver compileTimeGlobalModelResolver() {
-        return new EmptyCompileTimeGlobalModelResolver();
-    }
-
-    @Bean
-    public GlobalModelResolver globalModelResolver() {
-        final DefaultGlobalModelResolver defaultGlobalModelResolver = new DefaultGlobalModelResolver();
-        defaultGlobalModelResolver.setResolvers(Lists.newArrayList(
-                new RequestParametersResolver(),
-                new RequestHeadersResolver(),
-                new CookieResolver(),
-                requestContextResolver(),
-                new HttpSessionResolver(),
-                webApplicationContextResolver(),
-                servletContextResolver()
-        ));
-
-        return defaultGlobalModelResolver;
-    }
-
-    @Bean
-    public ServletContextResolver servletContextResolver() {
-        return new ServletContextResolver();
-    }
-
-    @Bean
-    public WebApplicationContextResolver webApplicationContextResolver() {
-        return new WebApplicationContextResolver();
-    }
-
-    @Bean
-    public RequestContextResolver requestContextResolver() {
-        return new RequestContextResolver();
-    }
-
-    @Bean
-    public LocaleProvider localeProvider() {
-        return new SpringLocaleProvider();
-    }
-
-    @Bean
-    public EmptySoyMsgBundleResolver soyMsgBundleResolver() {
-        return new EmptySoyMsgBundleResolver();
-    }
-
-    @Bean
-    public ToSoyDataConverter toSoyDataConverter() {
-        return new DefaultToSoyDataConverter();
-    }
-
-    public TemplateRenderer templateRenderer() {
-        final DefaultTemplateRenderer defaultTemplateRenderer = new DefaultTemplateRenderer();
-        defaultTemplateRenderer.setToSoyDataConverter(toSoyDataConverter());
-        defaultTemplateRenderer.setDebugOn(debugOn);
-
-        return defaultTemplateRenderer;
-    }
-
-    @Bean
-    public TofuCompiler tofuCompiler() {
-        final DefaultTofuCompiler defaultTofuCompiler = new DefaultTofuCompiler();
-        defaultTofuCompiler.setCompileTimeGlobalModelResolver(compileTimeGlobalModelResolver());
-        defaultTofuCompiler.setDebugOn(debugOn);
-
-        return defaultTofuCompiler;
-    }
-
-    @Bean
-    public ViewResolver viewResolver() throws Exception {
-        final SoyTemplateViewResolver soyTemplateViewResolver = new SoyTemplateViewResolver();
-        soyTemplateViewResolver.setDebugOn(debugOn);
-        soyTemplateViewResolver.setTemplateRenderer(templateRenderer());
-        soyTemplateViewResolver.setGlobalModelResolver(globalModelResolver());
-        soyTemplateViewResolver.setLocaleProvider(localeProvider());
-        soyTemplateViewResolver.setSoyMsgBundleResolver(soyMsgBundleResolver());
-        soyTemplateViewResolver.setCompiledTemplatesHolder(compiledTemplatesHolder());
-
-        return soyTemplateViewResolver;
-    }
-
-    @Bean
-    public CompiledTemplatesHolder compiledTemplatesHolder() throws Exception {
-        final DefaultCompiledTemplatesHolder defaultCompiledTemplatesHolder = new DefaultCompiledTemplatesHolder();
-        defaultCompiledTemplatesHolder.setDebugOn(debugOn);
-        defaultCompiledTemplatesHolder.setTemplatesFileResolver(templateFilesResolver());
-        defaultCompiledTemplatesHolder.setTofuCompiler(tofuCompiler());
-
-        return defaultCompiledTemplatesHolder;
-    }
-
-    @Bean
-    public OutputProcessor closureCompilerProcessor() {
-        return new GoogleClosureOutputProcessor();
+    @Primary
+    public static ModelAdjuster modelAdjuster() {
+        return new EmptyModelAdjuster();
     }
 
     @Bean
     public ViewResolver contentNegotiatingViewResolver() throws Exception {
         final ContentNegotiatingViewResolver contentNegotiatingViewResolver = new ContentNegotiatingViewResolver();
-        contentNegotiatingViewResolver.setViewResolvers(Lists.newArrayList(viewResolver()));
+        contentNegotiatingViewResolver.setViewResolvers(Lists.newArrayList(viewResolver));
         contentNegotiatingViewResolver.setDefaultViews(Lists.<View>newArrayList(new MappingJacksonJsonView()));
 
         return contentNegotiatingViewResolver;
     }
 
     @Bean
-    public TemplateUrlComposer templateUrlComposer() throws Exception {
-        final DefaultTemplateUrlComposer urlComposer = new DefaultTemplateUrlComposer();
-        urlComposer.setSiteUrl("http://localhost:8080/spring-soy-view-example/app");
-        urlComposer.setTemplateFilesResolver(templateFilesResolver());
-        urlComposer.setHashFileGenerator(hashFileGenerator());
-
-        return urlComposer;
-    }
-
-    @Bean
-    public HashFileGenerator hashFileGenerator() {
-        final MD5HashFileGenerator md5HashFileGenerator = new MD5HashFileGenerator();
-        md5HashFileGenerator.setDebugOn(debugOn);
-        md5HashFileGenerator.afterPropertiesSet();
-
-        return md5HashFileGenerator;
-    }
-
-    @Bean
-    public SoyAjaxController soyAjaxController() throws Exception {
-        final SoyAjaxController soyAjaxController = new SoyAjaxController();
-        soyAjaxController.setDebugOn(debugOn);
-        soyAjaxController.setLocaleProvider(localeProvider());
-        soyAjaxController.setSoyMsgBundleResolver(soyMsgBundleResolver());
-        soyAjaxController.setTemplateFilesResolver(templateFilesResolver());
-        soyAjaxController.setTofuCompiler(tofuCompiler());
-        soyAjaxController.setOutputProcessors(Lists.newArrayList(closureCompilerProcessor()));
-        soyAjaxController.setAuthManager(authManager());
-
-        return soyAjaxController;
-    }
-
-    @Bean
+    @Primary
     public AuthManager authManager() {
-        return new ConfigurableAuthManager(Lists.newArrayList("templates/client-words.soy", "templates/server-time.soy"));
+        return new ConfigurableAuthManager(
+                Lists.newArrayList(
+                "templates/client-words.soy",
+                "templates/server-time.soy")
+        );
     }
 
 }
