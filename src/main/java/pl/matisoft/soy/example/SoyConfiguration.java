cef6f115f0283b168c1d8a88c97d5b5c7a1d9fcc
Mateusz Szczap <mszczap@team.mobile.de>
2013-11-12 00:09:44 +0100

    updated examples
    Change-Id: I5fe491967142fc1baddde942d29b1dff5473b744

--- src/main/java/pl/matisoft/soy/example/SoyConfiguration.java
+++ src/main/java/pl/matisoft/soy/example/SoyConfiguration.java
@@ -1,6 +1,7 @@
 package pl.matisoft.soy.example;
 
-import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.google.common.collect.Lists;
 import org.springframework.context.annotation.*;
@@ -13,6 +14,11 @@ import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.json.MappingJacksonJsonView;
 import pl.matisoft.soy.ajax.auth.AuthManager;
 import pl.matisoft.soy.ajax.auth.ConfigurableAuthManager;
+import pl.matisoft.soy.global.compile.CompileTimeGlobalModelResolver;
+import pl.matisoft.soy.global.compile.DefaultCompileTimeGlobalModelResolver;
+import pl.matisoft.soy.global.runtime.DefaultGlobalModelResolver;
+import pl.matisoft.soy.global.runtime.GlobalModelResolver;
+import pl.matisoft.soy.global.runtime.resolvers.RuntimeDataResolver;
 
 /**
  * Created with IntelliJ IDEA.
@@ -22,13 +28,11 @@ import pl.matisoft.soy.ajax.auth.ConfigurableAuthManager;
  */
 @Configuration
 @ImportResource({"classpath:spring-soy-view-ajax-config.xml"})
-@PropertySource("classpath:/spring-soy-view-example.properties")
+@PropertySource("classpath:spring-soy-view-example.properties")
 @EnableWebMvc
 @ComponentScan(basePackages = {"pl.matisoft.soy.example"})
+//http://www.aviransplace.com/2012/04/29/how-to-use-spring-java-config/
 public class SoyConfiguration extends WebMvcConfigurerAdapter {
-
-    @Resource(name = "soyViewResolver")
-    private ViewResolver viewResolver;
 
     @Bean
     public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
@@ -36,9 +40,10 @@ public class SoyConfiguration extends WebMvcConfigurerAdapter {
     }
 
     @Bean
-    public ViewResolver contentNegotiatingViewResolver() throws Exception {
+    @Primary
+    public ViewResolver contentNegotiatingViewResolver(final ViewResolver soyViewResolver) throws Exception {
         final ContentNegotiatingViewResolver contentNegotiatingViewResolver = new ContentNegotiatingViewResolver();
-        contentNegotiatingViewResolver.setViewResolvers(Lists.newArrayList(viewResolver));
+        contentNegotiatingViewResolver.setViewResolvers(Lists.newArrayList(soyViewResolver));
         contentNegotiatingViewResolver.setDefaultViews(Lists.<View>newArrayList(new MappingJacksonJsonView()));
 
         return contentNegotiatingViewResolver;
@@ -46,11 +51,32 @@ public class SoyConfiguration extends WebMvcConfigurerAdapter {
 
     @Bean
     @Primary
+    public GlobalModelResolver globalModelResolver(final GlobalModelResolver resolver) {
+        final DefaultGlobalModelResolver original = (DefaultGlobalModelResolver) resolver;
+        return new DefaultGlobalModelResolver(original.getResolvers(),
+                        Lists.<RuntimeDataResolver>newArrayList(
+                                new ExampleRuntimeDataResolver()
+                        ));
+    }
+
+    @Bean
+    @Primary
+    public CompileTimeGlobalModelResolver compileTimeVars() {
+        final Map data = new HashMap();
+        data.put("test1", "testValue1");
+        final DefaultCompileTimeGlobalModelResolver defaultCompileTimeGlobalModelResolver = new DefaultCompileTimeGlobalModelResolver();
+        defaultCompileTimeGlobalModelResolver.setData(data);
+
+        return defaultCompileTimeGlobalModelResolver;
+    }
+
+    @Bean
+    @Primary
     public AuthManager authManager() {
         return new ConfigurableAuthManager(
                 Lists.newArrayList(
-                "templates/client-words.soy",
-                "templates/server-time.soy")
+                        "templates/client-words.soy",
+                        "templates/server-time.soy")
         );
     }
 
